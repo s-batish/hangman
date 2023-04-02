1b57ac1da8dafb70cb92accf65b9039733a84067
s-batish <shivanibatish@btinternet.com>
2023-04-02 16:24:07 +0000

    Add while loop to welcome() for menu choices

--- run.py
+++ run.py
@@ -18,13 +18,30 @@ def welcome():
     """
     Prints welcome message and gives user 3 options:
     to play the game, to see the rules and to go to the
-    high scores area
+    high scores area.
+    Run a while loop to collect a valid input from user.
+    The loop will continue to run until user enters valid
+    data.
     """
     print("Welcome to your game of Hangman!\n")
     print("""Select an option (1, 2 or 3) to continue:
     1 - Play Hangman
     2 - Read the rules
-    3 - See the high scores""")
+    3 - See the high scores\n""")
+
+    while True:
+        menu_options = input("Select an option: ").strip()
+        if menu_options == "1":
+            print("You selected 1")
+            break
+        elif menu_options == "2":
+            print("You selected 2")
+            break
+        elif menu_options == "3":
+            print("You selected 3")
+            break
+        else:
+            print("Please only enter number 1, 2 or 3\n")        
 
 def main():
     """
